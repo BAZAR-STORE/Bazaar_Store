ad50e29e3d405bfe9541c0c84619f41e15911389
Haneen M Al-Hamdan <henno_hamdan@yahoo.com>
2022-05-30 23:49:22 +0300

    Add Models

---
+++ Bazaar_Store/Models/Prodect.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bazaar_Store.Models
+{
+    public class Prodect
+    {
+        public int Id { get; set; }
+        [Required]
+        public double InStok { get; set; }
+        [Required]
+        public long BarCode { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [Required]
+        public bool DiscountPrice { get; set; }
+        [Required]
+        public string ProductDesciption { get; set; }
+        [Required]
+        public bool TodaysDeals { get; set; }
+
+        public int CompanyId { get; set; }
+
+    }
+}
