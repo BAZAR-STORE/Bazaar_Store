b072c6b673f235775cd3bc66e50a469b07b4bdb8
Haneen M Al-Hamdan <henno_hamdan@yahoo.com>
2022-05-31 08:48:48 +0300

    Add Data

---
+++ Bazaar_Store/Models/Interface/IAdmin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bazaar_Store.Models.Interface
+{
+    public interface IAdmin
+    {
+    }
+}
